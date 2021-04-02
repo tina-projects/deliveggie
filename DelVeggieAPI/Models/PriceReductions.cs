792261e0f57afb6b2469f19aadf34b4ac582f834
Tina <tina_kgeorge@yahoo.co.in>
2021-04-02 22:52:17 +0530

    Preice reduction model and sample data

---
+++ DelVeggieAPI/Models/PriceReductions.cs
@@ -0,0 +1,15 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DelVeggieAPI
+{
+    public class PriceReductions
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public int DayOfWeek { get; set; }
+        public double Reduction { get; set; }
+    }
+}
